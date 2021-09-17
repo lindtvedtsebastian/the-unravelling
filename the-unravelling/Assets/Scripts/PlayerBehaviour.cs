e85f2c1de4b1b79ff5f939e332d865d5079b4418
Dennis Kristiansen <denniskristiansen@protonmail.com>
2021-09-17 17:15:35 +0200

    Add input handlers for player input

--- the-unravelling/Assets/Scripts/PlayerBehaviour.cs
+++ the-unravelling/Assets/Scripts/PlayerBehaviour.cs
@@ -7,20 +7,55 @@ public class PlayerBehaviour : MonoBehaviour {
     // The speed of the players movement
     public float speed = 200.0f;
 
+    // Components
     private Rigidbody2D body;
     private InputAction moveAction;
 
-    // Start is called before the first frame update
-    void Start() {
+    // Initialize the components
+    private void Awake() {
         body = GetComponent<Rigidbody2D>();
 
-        moveAction = GetComponent<PlayerInput>().actions["Move"];
-        moveAction.Enable();
+        var actions = GetComponent<PlayerInput>().actions;
+
+        // Grab a ref to move action, so we can read it later
+        moveAction = actions["Move"];
+
+        // Setup action handlers
+        actions["Inventory"].performed += OnActionInventory;
+        actions["Interact"].performed += OnActionInteract;
+        actions["Place"].performed += OnActionPlace;
+        actions["Cancel"].performed += OnActionCancel;
+        actions["Destroy"].performed += OnActionDestroy;
     }
 
     private void FixedUpdate() {
         var move = moveAction.ReadValue<Vector2>();
 
         body.velocity = move * (Time.deltaTime * speed);
+    }
+
+    // Called when inventory action is triggered
+    private void OnActionInventory(InputAction.CallbackContext ctx) {
+        Debug.Log("Open Inventory");
+    }
+
+    // Called when interact action is triggered
+    private void OnActionInteract(InputAction.CallbackContext ctx) {
+        Debug.Log("Interact with...");
+    }
+
+    // Called when place action is triggered
+    private void OnActionPlace(InputAction.CallbackContext ctx) {
+        Debug.Log("Place tile/machine");
+    }
+
+    // Called when cancel action is triggered
+    private void OnActionCancel(InputAction.CallbackContext ctx) {
+        Debug.Log("Cancel current action");
+    }
+
+    // Called when destroy action is triggered
+    private void OnActionDestroy(InputAction.CallbackContext ctx) {
+        Debug.Log("Destroy tile/machine");
     }
 }
