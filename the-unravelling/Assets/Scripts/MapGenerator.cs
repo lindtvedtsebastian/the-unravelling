fda69e40ac6141ff3a70b90ec91b72f056d20a48
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-09-17 15:45:28 +0200

    Added a function for basic noise generation

--- the-unravelling/Assets/Scripts/MapGenerator.cs
+++ the-unravelling/Assets/Scripts/MapGenerator.cs
@@ -2,17 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapGenerator : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public static class MapGenerator {
+    private const int maxValue = 100000;
+    private const int minValue = -100000;
+    public static float[,] GenerateNoiseMap(int mapSize, int seed, float scale,
+        int octaves, float persistance, float lacunarity, Vector2 offset) {
+        float[,] noiseMap = new float[mapSize, mapSize];
+        System.Random pseudo_rng = new System.Random(seed);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        for (int i = 0; i < octaves; i++) {
+            float offsetX = pseudo_rng.Next(minValue, maxValue) + offset.x;
+            float offsetY = pseudo_rng.Next(minValue, maxValue) + offset.y;
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
+
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
+        float halfSize = mapSize / 2f;
+
+        for (int y = 0; y < mapSize; y++) {
+            for (int x = 0; x < mapSize; x++) {
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
+
+                for (int i = 0; i < octaves; i++) {
+                    float sampleX = (x - halfSize) / scale * frequency + octaveOffsets[i].x;
+                    float sampleY = (y - halfSize) / scale * frequency + octaveOffsets[i].y;
+
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                    noiseHeight += perlinValue * amplitude;
+
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+                }
+
+                if (noiseHeight > maxNoiseHeight)
+                    maxNoiseHeight = noiseHeight;
+                if (noiseHeight > minNoiseHeight)
+                    minNoiseHeight = noiseHeight;
+                noiseMap[x, y] = noiseHeight;
+            }
+        }
+
+        for (int y = 0; y < mapSize; y++) {
+            for (int x = 0; x < mapSize; x++) {
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
+            }
+        }
+
+        return noiseMap;
     }
 }
