5fae98c15eb12ad08687d1efbaae6c017dbbeb44
Dennis Kristiansen <denniskristiansen@protonmail.com>
2021-09-17 14:12:43 +0200

    Reformat

--- the-unravelling/Assets/Scripts/PlayerBehaviour.cs
+++ the-unravelling/Assets/Scripts/PlayerBehaviour.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerBehaviour : MonoBehaviour
-{
+public class PlayerBehaviour : MonoBehaviour {
     // The speed of the players movement
     public float speed = 200.0f;
 
@@ -12,16 +11,14 @@ public class PlayerBehaviour : MonoBehaviour
     private InputAction moveAction;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         body = GetComponent<Rigidbody2D>();
 
         moveAction = GetComponent<PlayerInput>().actions["Move"];
         moveAction.Enable();
     }
 
-    private void FixedUpdate()
-    {
+    private void FixedUpdate() {
         var move = moveAction.ReadValue<Vector2>();
 
         body.velocity = move * (Time.deltaTime * speed);
