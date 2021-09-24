7c42904248d796fa91ba6f45b978887ef4845e4e
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-09-24 11:10:01 +0200

    Removed uneccesary imports in ScribtableObjectSingelton

--- the-unravelling/Assets/Scripts/ScriptableObjectSingleton.cs
+++ the-unravelling/Assets/Scripts/ScriptableObjectSingleton.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ScriptableObjectSingleton<T> : ScriptableObject where T : ScriptableObjectSingleton<T> {
