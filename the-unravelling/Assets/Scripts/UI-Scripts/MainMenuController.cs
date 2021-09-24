3465e27980e9ea3318012a8cc890c23a07a812a7
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-09-24 09:00:36 +0200

    Added basic support for seeding world generation

--- the-unravelling/Assets/Scripts/UI-Scripts/MainMenuController.cs
+++ the-unravelling/Assets/Scripts/UI-Scripts/MainMenuController.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour {
     public InputField newWorldName;
+    public InputField newWorldSeed;
     public MapPreview mapPreview;
     // Start is called before the first frame update
     public void exitButton() {
@@ -16,7 +17,10 @@ public class MainMenuController : MonoBehaviour {
     }
 
     public void generateMap() {
-        MapGenerator.GenerateNoiseMap(newWorldName.text,64,1,50f,6,0.5f,2f,new Vector2(0,0));
+        int seed = newWorldSeed.text.GetHashCode();
+        if (newWorldSeed.text == "")
+            seed = new System.Random().Next(0,1_000_000);
+        MapGenerator.GenerateNoiseMap(newWorldName.text,64,seed,50f,6,0.5f,2f,new Vector2(0,0));
         mapPreview.drawMap();
         newWorldName.text = "Enter game world name";
     }
