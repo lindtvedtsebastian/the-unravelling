a5ec6b5ddffd81db74962edd2bd8174a7c7e50fb
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-10-01 19:58:43 +0200

    Preview of loaded file implemented

--- the-unravelling/Assets/Scripts/UI-Scripts/LoadGameAddSave.cs
+++ the-unravelling/Assets/Scripts/UI-Scripts/LoadGameAddSave.cs
@@ -8,7 +8,7 @@ using System.IO;
 
 public class LoadGameAddSave : MonoBehaviour
 {
-    private List<string> stringData = new List<string>();
+    private List<string> fileNames = new List<string>();
     private DirectoryInfo dir;
     private FileInfo[] files;
     public GameObject buttonPrefab;
@@ -20,24 +20,28 @@ public class LoadGameAddSave : MonoBehaviour
         files = dir.GetFiles("*.png");
 
         foreach (FileInfo file in files) { // fill the entries with random data for now until savefiles are a thing.
-            stringData.Add(file.Name.Replace(".png",""));
+            fileNames.Add(file.Name);
         }
 
-        for (int i = 0; i < stringData.Count; i++) {
+        for (int i = 0; i < fileNames.Count; i++) {
             GameObject newButton = Instantiate(buttonPrefab);
+            string fileName = fileNames[i];
+            string worldName = fileNames[i].Replace(".png", "");
             newButton.transform.SetParent(transform,false);
-            newButton.name = stringData[i];
-            newButton.GetComponentInChildren<TMP_Text>().text = stringData[i];
-            newButton.GetComponent<Button>().onClick.AddListener(SelectSave);
+            newButton.name = worldName;
+            newButton.GetComponentInChildren<TMP_Text>().text = worldName;
+            newButton.GetComponent<Button>().onClick.AddListener(() => SelectSave(fileName));
         }
     }
 
     /// <summary>
-    /// SelectSave triggers the event for changing the preview image and sets the current selected button as savefile to load.
+    /// SelectSave triggers the event for changing the preview image and sets
+    /// the current selected button as savefile to load.
     /// </summary>
-    public void SelectSave() {
-        // byte[] image =
-        previewImage.GetComponent<RawImage>().color = Color.gray;
-        // ImageConversion.LoadImage("");
+    public void SelectSave(string fileName) {
+        byte[] image = File.ReadAllBytes(Application.persistentDataPath + "/" + fileName);
+        Texture2D tex = new Texture2D(1, 1); // Size does not matter, will be overwritten
+        tex.LoadImage(image);
+        previewImage.GetComponent<RawImage>().texture = tex;
     }
 }
