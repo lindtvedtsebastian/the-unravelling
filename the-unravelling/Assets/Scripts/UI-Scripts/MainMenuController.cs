3357d924a7c1e9a364f14f13a96ea859f571c88c
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-10-02 18:00:56 +0200

    Overhauled Seed system, button for generating seed added

--- the-unravelling/Assets/Scripts/UI-Scripts/MainMenuController.cs
+++ the-unravelling/Assets/Scripts/UI-Scripts/MainMenuController.cs
@@ -10,6 +10,7 @@ public class MainMenuController : MonoBehaviour {
 
 
     void Start() {
+        randomSeed();
         MapSizeChanged(mapSize);
         mapSize.onValueChanged.AddListener(delegate {
             MapSizeChanged(mapSize);
@@ -22,16 +23,14 @@ public class MainMenuController : MonoBehaviour {
     }
     public void startGame() {
         if (GameData.Get.world.map == null) {
-            Debug.LogError("Invalid game data, did you press generate?"); // NOTE: This should most likely pop up/active an little ui window instead.
+            Debug.LogError("Invalid game data, did you press generate?"); // Change to ingame message instead
             return;
         }
         SceneManager.LoadScene("MainGame");
     }
 
     public void generateMap() {
-        int seed = newWorldSeed.text.GetHashCode();
-        if (newWorldSeed.text == "")
-            seed = new System.Random().Next(0,1_000_000);
+        int seed = int.Parse(newWorldSeed.text); // Know that content type is int 
         MapGenerator.GenerateTilemap(newWorldName.text,GameData.Get.world.worldSize,seed,50f,6,0.5f,2f,new Vector2(0,0));
         mapPreview.drawMap();
         newWorldName.text = "";
@@ -43,5 +42,10 @@ public class MainMenuController : MonoBehaviour {
             case 1: GameData.Get.world.worldSize = 512; break;
             case 2: GameData.Get.world.worldSize = 1024; break;
         }
+    }
+
+    public void randomSeed() {
+        int seed = new System.Random().Next(0, 999_999_999);
+        newWorldSeed.text = seed.ToString();
     }
 }
