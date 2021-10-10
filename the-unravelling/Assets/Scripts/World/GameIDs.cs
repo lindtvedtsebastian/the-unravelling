fd02a1370bea9dba01b2924db7a7e58905806873
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-10-10 21:00:36 +0200

    Doxygen GameIDs

--- the-unravelling/Assets/Scripts/World/GameIDs.cs
+++ the-unravelling/Assets/Scripts/World/GameIDs.cs
@@ -1,4 +1,7 @@
 
+//// <summary>
+/// A static class that contains all the various gameIDs for easy access
+/// </summary>
 public static class GameIDs {
     public const int GRASS = 0;
     public const int DIRT  = 1;
