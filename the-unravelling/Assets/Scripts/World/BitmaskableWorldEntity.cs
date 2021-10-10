03f53b3b3e437631f7e345325800ab538a6d89b1
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-10-10 23:51:11 +0200

    All entity classes doxygen commented

--- the-unravelling/Assets/Scripts/World/BitmaskableWorldEntity.cs
+++ the-unravelling/Assets/Scripts/World/BitmaskableWorldEntity.cs
@@ -16,11 +16,22 @@ public class BitmaskableWorldEntity : WorldEntity
     private bool eastExists;
     private bool southExists;
 
-
+    /// <summary>
+    /// Determines what sprite should be at this given location [x,y]
+    /// </summary>
+    /// <param name="y">The tile's position on the y axis</param>
+    /// <param name="x">The tile's position on the x axis</param>
+    /// <returns>The sprite that should be at this world position</returns>
     public override TileBase SetSprite(int y, int x) {
         return this.sprites[bitmaskToSpriteIndex[calculateBitmask(y,x)]];
     }
 
+    /// <summary>
+    /// Calculates the bitmask for the tile at position [x,y]
+    /// </summary>
+    /// <param name="y">The tile's position on the y axis</param>
+    /// <param name="x">The tile's position on the x axis</param>
+    /// <returns>The bitmask (int value) at the tile's position</returns>
     public int calculateBitmask(int y, int x) {
         resetBooleans();
         identifyDirections(y, x);
@@ -29,6 +40,11 @@ public class BitmaskableWorldEntity : WorldEntity
         return bitmask;
     }
 
+    /// <summary>
+    /// Determines which directions are relevant for this tile's calculations
+    /// </summary>
+    /// <param name="y">The tile's position on the y axis</param>
+    /// <param name="x">The tile's position on the x axis</param>
     private void identifyDirections(int y, int x) {
         checkNorth = y - 1 >= 0;
         checkWest = x - 1 >= 0;
@@ -37,57 +53,86 @@ public class BitmaskableWorldEntity : WorldEntity
     }
 
 
+    /// <summary>
+    /// Calculates the bitmask for this tile's cardinals
+    /// </summary>
+    /// <param name="y">The tile's position on the y axis</param>
+    /// <param name="x">The tile's position on the x axis</param>
+    /// <returns>The calculated bitmask sum for the cardinal directions</returns>
     public int calculateCardinals(int y, int x) {
         int bitmask = 0;
-        if (checkNorth && WorldCheck(y-1,x,this.id)) {
+        if (checkNorth && BitmaskPredicate(y-1,x,this.id)) {
             bitmask += GameData.N;
             northExists = true;
         }
-        if (checkWest && WorldCheck(y,x-1,this.id)) {
+        if (checkWest && BitmaskPredicate(y,x-1,this.id)) {
             bitmask += GameData.W;
             westExists = true;
         }
-        if (checkEast && WorldCheck(y,x+1,this.id)) {
+        if (checkEast && BitmaskPredicate(y,x+1,this.id)) {
             bitmask += GameData.E;
             eastExists = true;
         }
-        if (checkSouth && WorldCheck(y+1,x,this.id)) {
+        if (checkSouth && BitmaskPredicate(y+1,x,this.id)) {
             bitmask += GameData.S;
             southExists = true;
         }
         return bitmask;
     }
 
+    /// <summary>
+    /// Calculates the bitmask for this tile's corners
+    /// </summary>
+    /// <param name="y">The tile's position on the y axis</param>
+    /// <param name="x">The tile's position on the y axis</param>
+    /// <returns>The calculated bitmask sum for the corners</returns>
     public int calculateCorners(int y, int x) {
         int bitmask = 0;
         
-        if ((checkNorth && checkWest && WorldCheck(y-1, x-1,this.id))
+        if ((checkNorth && checkWest && BitmaskPredicate(y-1, x-1,this.id))
             && northExists && westExists) {
             bitmask += GameData.NW;
         }
-        if ((checkNorth && checkEast && WorldCheck(y-1, x+1,this.id))
+        if ((checkNorth && checkEast && BitmaskPredicate(y-1, x+1,this.id))
             && northExists && eastExists) {
             bitmask += GameData.NE;
         }
-        if ((checkSouth && checkWest && WorldCheck(y+1, x-1,this.id))
+        if ((checkSouth && checkWest && BitmaskPredicate(y+1, x-1,this.id))
             && southExists && westExists) {
             bitmask += GameData.SW;
         }
-        if ((checkSouth && checkEast && WorldCheck(y+1, x+1,this.id))
+        if ((checkSouth && checkEast && BitmaskPredicate(y+1, x+1,this.id))
             && southExists && eastExists) {
             bitmask += GameData.SE;
         }
         return bitmask;
     }
 
+    /// <summary>
+    /// Checks if the tile at the provided x and y axis is of the provided id or not
+    /// </summary>
+    /// <param name="y">The position of the tile to be checked in the y axis</param>
+    /// <param name="x">The position of the tile to be checked in the x axis</param>
+    /// <param name="id">The entity ID to be checked against the position in the world</param>
+    /// <returns>Wheter or not the tile at pos [x,y] is of the provided entity ID</returns>
     public bool IsWorldPosTile(int y, int x, int id) {
         return GameData.Get.world.map[y, x] == id;
     }
 
-    public virtual bool WorldCheck(int y, int x, int id) {
+    /// <summary>
+    /// The base function for determining if a tile should be part of the bitmask or not
+    /// <summary>
+    /// <param name="y">The position of the tile to be checked in the y axis</param>
+    /// <param name="x">The position of the tile to be checked in the x axis</param>
+    /// <param name="id">The id of the tile having its mask calculated</param>
+    /// <returns>Wheter or not the tile that was checked is part of the bitmask or not</returns>
+    public virtual bool BitmaskPredicate(int y, int x, int id) {
         return IsWorldPosTile(y, x, id);
     }
 
+    /// <summary>
+    /// Resets all the booleans used for the bitmask calculation 
+    /// </summary>
     private void resetBooleans() {
         checkNorth = false;
         checkWest = false;
