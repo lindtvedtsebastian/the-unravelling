f440fc52d35e57abe227ec800785361ec9e99389
salvadorbascunan <bascunan.salvador@gmail.com>
2021-10-25 20:35:19 +0200

    created slots for crafting items

---
+++ the-unravelling/Assets/Scripts/CraftingSlot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CraftingSlot : MonoBehaviour {
+    ItemData craftItem;
+
+    public Image craftingImg;
+    public Text craftingNum;
+
+    public void AddCraftingItem(ItemData newCraftItem)
+    {
+        craftItem = newCraftItem;
+        craftingImg.sprite = craftItem.preview;
+        craftingImg.enabled = true;
+        craftingNum.enabled = true;
+        craftingNum.text = craftItem.itemAmount.ToString();
+    }
+
+}
