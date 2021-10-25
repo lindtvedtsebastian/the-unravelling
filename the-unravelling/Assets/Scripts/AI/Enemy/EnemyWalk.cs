184061289ef588e9e7b37e2f91a1bfa1178ba686
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-10-25 21:46:53 +0200

    Small improvements and docstrings added

--- the-unravelling/Assets/Scripts/AI/Enemy/EnemyWalk.cs
+++ the-unravelling/Assets/Scripts/AI/Enemy/EnemyWalk.cs
@@ -1,22 +1,34 @@
-using System.Collections;
 using Unity.Collections;
 using Unity.Mathematics;
 using UnityEngine;
 
+/// <summary>
+/// Handles enemy movement
+/// </summary>
 public class EnemyWalk : State 
 {
     private NativeList<PathPart> _resultPath;
     public GameObject _player;
 
+    private float speed = 3f;
+    private float proximityRange = 0.5f;
+
+    /// <summary>
+    /// All necessary preparations before "do"ing the state 
+    /// </summary>
+    /// <param name="stateManager">This state's manager</param>
     public override void EnterState(StateManager stateManager) {
         _resultPath = new NativeList<PathPart>(Allocator.Persistent);
 
         _player = GameObject.FindGameObjectWithTag("Player");
         _stateManager = stateManager;
     }
-    
+
+    /// <summary>
+    /// The state "update" loop 
+    /// </summary>
     public override void DoState() {
-		if (distanceTo(_player.transform.position) > 0.5f) {
+		if (distanceTo(_player.transform.position) > proximityRange) {
         // If no path exists, calculate one
         if (_resultPath.Length <= 0)
             CalculatePath();
@@ -25,10 +37,16 @@ public class EnemyWalk : State
 		}
     }
 
+    /// <summary>
+    /// Prepares the State for exit
+    /// </summary>
     public override void LeaveState() {
         _resultPath.Dispose();
     }
 
+    /// <summary>
+    /// Calculates a new pathfinding path
+    /// </summary>
     private void CalculatePath() {
         Vector3 enemyPos = gameObject.transform.position;
         Vector3 playerPos = _player.transform.position;
@@ -36,9 +54,13 @@ public class EnemyWalk : State
         int2 startPos = new int2(intRound(enemyPos.x), intRound(enemyPos.y));
         int2 endPos = new int2(intRound(playerPos.x), intRound(playerPos.y));
 
+        _resultPath.Clear();
         Pathfinding pathfinding = new Pathfinding(startPos, endPos, _resultPath);
 	}
 
+    /// <summary>
+    /// Moves along the calculated path
+    /// </summary>
     private void Move() {
 		if (_resultPath.Length > 0) {
             PathPart currentWaypoint = _resultPath[_resultPath.Length - 1];
@@ -48,23 +70,38 @@ public class EnemyWalk : State
 				// Destroy structure
 			} else {
                 moveTowards(target);
-                if (distanceTo(target) < 0.5f)
+                if (distanceTo(target) < proximityRange)
 					_resultPath.RemoveAt(_resultPath.Length - 1);
             }
         }
 	}
 
-	int intRound(float toBeRounded) {
+    /// <summary>
+    /// Helper function for rounding a float to the closest int 
+    /// </summary>
+    /// <param name="toBeRounded">The float to be rounded</param>
+    /// <returns>The provided float converted to the closest int</returns>
+    int intRound(float toBeRounded) {
         return (int) Mathf.Round(toBeRounded);
     }
 
-	void moveTowards(Vector3 target) {
+
+    /// <summary>
+    /// Moves this gameObject towards a given vector3 target location 
+    /// </summary>
+    /// <param name="target">The Vector3 point of which to move towards</param>
+    void moveTowards(Vector3 target) {
 		gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position,
 															target,
-															3f * Time.deltaTime);
+															speed * Time.deltaTime);
 	}
-	
-	float distanceTo(Vector3 target) {
+
+    /// <summary>
+    /// Calculates the distance from this gameObject to a given target 
+    /// </summary>
+    /// <param name="target">The Vector3 of which to calculate the distance to</param>
+    /// <returns>The distance (a float number) between this gameObject and the target</returns>
+    float distanceTo(Vector3 target) {
         return Vector3.Distance(gameObject.transform.position, target);
     }
 }
