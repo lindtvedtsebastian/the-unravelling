f06a5d5bb0935a97693df87e013b0fa4aa90b8d1
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-10-26 20:56:07 +0200

    New recipe system

---
+++ the-unravelling/Assets/Scripts/Inventory/CraftingData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(fileName = "CraftingData", menuName = "Crafting/CraftingData", order = 2)]
+public class CraftingData : ScriptableObjectSingleton<CraftingData> {
+    public CraftingRecipe[] craftingRecipes;
+}
+
+[CreateAssetMenu(fileName = "Crafting recipe", menuName = "Crafting/Crafting recipe"]
+ public class CraftingRecipe : ScriptableObject {
+    public int id;
+    public string recipeName;
+    public int resultingAmount;
+    public RecipeData[] recipeItems; 
+}
+
+ [Serializable]
+ public class RecipeData {
+    public ItemData item;
+    public int amount; 
+}
