4c1bc779c0c500df36deaede8272ee7bb7c52cff
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-10-26 10:07:09 +0200

    docstring wave, a collection of waves in wavemanager

--- the-unravelling/Assets/Scripts/World/Wave.cs
+++ the-unravelling/Assets/Scripts/World/Wave.cs
@@ -1,20 +1,30 @@
 using System;
 using UnityEngine;
 
+/// <summary>
+/// Defines how a wave should be  
+/// </summary>
+[Serializable]
 [CreateAssetMenu(fileName = "Wave", menuName = "Wave")]
 public class Wave : ScriptableObject {
     public WaveEnemy[] waveEnemies;
-    public Direction waveDirection;
+    public AttackDirection waveDirection;
+    public int maxConcurrentEnemies;
 }
 
-
+/// <summary>
+/// The enemy prefab to be spawned, and the amount of that prefab
+/// </summary>
 [Serializable]
 public class WaveEnemy {
     public GameObject enemy;
     public int count;
 }
 
-public enum Direction {
+/// <summary>
+/// In what direction the enemies of the wave should attack from 
+/// </summary>
+public enum AttackDirection {
     NORTH,
     SOUTH,
     EAST,
--- the-unravelling/Assets/Scripts/World/WaveManager.cs
+++ the-unravelling/Assets/Scripts/World/WaveManager.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaveManager : MonoBehaviour
-{
+public class WaveManager : MonoBehaviour {
+	[SerializeField]
+	private Wave[] waves;
+	
     // Start is called before the first frame update
     void Start()
     {
