df91f3f4eeb0064463b8104b13aa42ebdf58fa67
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-10-26 23:20:44 +0200

    Function for calculating the amount of times a recipe can be crafted

--- the-unravelling/Assets/Scripts/Inventory/Inventory.cs
+++ the-unravelling/Assets/Scripts/Inventory/Inventory.cs
@@ -57,6 +57,32 @@ public class Inventory : MonoBehaviour {
         return -1;
     }
 
+
+    /// <summary>
+    /// Determines if a recipe can be crafted with the contents of the inventory.
+    /// If the recipe can be crafted, the amount will be returned, if not -1 will be returned
+    /// </summary>
+    /// <param name="recipe">The recipe to be checked against the inventory</param>
+    /// <returns>How many times a recipe can be crafted, or -1 if not</returns>
+    int calculateRecipeCraftingAmount(CraftingRecipe recipe) {
+        int currentLowestCraftingAmount = int.MaxValue;
+        foreach (RecipeData data in recipe.recipeItems) {
+            int itemIndex = findItemDataIndex(data.item);
+			
+			// If item does not exist in inventory, return
+			if (itemIndex < 0) return -1; 
+			
+            float invAmount = (float) items[itemIndex].amount;
+            int currentCraftingAmount = (int) Math.Floor(invAmount / data.amount);
+
+			if (currentCraftingAmount < currentLowestCraftingAmount) {
+                currentLowestCraftingAmount = currentCraftingAmount;
+            }
+        }
+		if (currentLowestCraftingAmount != int.MaxValue) {
+            return currentLowestCraftingAmount;
+        } else return -1;
+    }
 }
 
 /// <summary>
