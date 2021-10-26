c047a360c50f60e1ab2898c3d66399f1b91fb986
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-10-26 21:18:34 +0200

    Work on crafting

--- the-unravelling/Assets/Scripts/Inventory/CraftingData.cs
+++ the-unravelling/Assets/Scripts/Inventory/CraftingData.cs
@@ -1,22 +1,22 @@
 ﻿using System;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Crafting Recipes", menuName = "Crafting/Crafting Recipes", order = 2)]
+[CreateAssetMenu(fileName = "Crafting Recipes", menuName = "Crafting/Crafting Recipes")]
 public class CraftingRecipes : ScriptableObjectSingleton<CraftingRecipes> {
     public CraftingRecipe[] craftingRecipes;
 }
 
 [Serializable]
-[CreateAssetMenu(fileName = "Crafting recipe", menuName = "Crafting/Crafting recipe")]
- public class CraftingRecipe : ScriptableObject {
+[CreateAssetMenu(fileName = "Crafting Recipe", menuName = "Crafting/Crafting recipe")]
+public class CraftingRecipe : ScriptableObject {
     public int id;
     public string recipeName;
     public int resultingAmount;
     public RecipeData[] recipeItems; 
 }
 
- [Serializable]
- public class RecipeData {
+[Serializable]
+public class RecipeData {
     public ItemData item;
     public int amount; 
 }
