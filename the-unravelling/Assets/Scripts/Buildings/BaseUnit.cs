06fc909cf7ab0dc3b7365f8941a5c6457a0705eb
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-10-27 09:22:40 +0200

    Removed duplicate files

--- the-unravelling/Assets/Scripts/Buildings/BaseUnit.cs
+++ the-unravelling/Assets/Scripts/Buildings/BaseUnit.cs
@@ -64,7 +64,7 @@ public class BaseUnit : MonoBehaviour {
     private void Drop() {
         // Find the player and get the inventory
         var player = GameObject.FindWithTag("Player");
-        var inventory = player.gameObject.GetComponent<PlayerBehaviour>().inventory;
+        // var inventory = player.gameObject.GetComponent<PlayerBehaviour>().inventory;
         
         // Add the dropped item
         var count = Random.Range(minDropCount, maxDropCount);
