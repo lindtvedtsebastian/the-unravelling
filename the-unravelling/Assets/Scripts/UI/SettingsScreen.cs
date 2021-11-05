990da694145d3dbd80a92e66d795d56ae50e302b
Elvis Arifagic <Slewision@gmail.com>
2021-11-05 15:20:06 +0100

    fetch resolutions from the operating systems connection to the monitor

--- the-unravelling/Assets/Scripts/UI/SettingsScreen.cs
+++ the-unravelling/Assets/Scripts/UI/SettingsScreen.cs
@@ -5,20 +5,44 @@ using UnityEngine.UI;
 using UnityEngine;
 using Vector2 = UnityEngine.Vector2;
 using TMPro;
+using UnityEngine.InputSystem.OnScreen;
 
 public class SettingsScreen : MonoBehaviour {
 
     public Toggle fullscreen;
     public Toggle vSync;
+
+    public TMP_Dropdown dropdown;
     
     public List<ScreenDimensions> resolutions = new List<ScreenDimensions>();
     private int currentResIndex;
 
+    private Resolution[] resolutionstest;
+    
     public TMP_Text resolutionLabelText;
 
     // Start is called before the first frame update
     void Start() {
-        fullscreen.isOn = Screen.fullScreen;
+        resolutionstest = Screen.resolutions;
+
+        List<string> chooseOptions = new List<string>();
+
+        var resIndex = 0;
+
+        for (int i = 0; i < resolutionstest.Length; i++) {
+            string test = resolutionstest[i].width + " x " + resolutionstest[i].height;
+            chooseOptions.Add(test);
+
+            if (resolutionstest[i].width == Screen.currentResolution.width && resolutionstest[i].height == Screen.currentResolution.height) {
+                resIndex = i;
+            }
+        }
+
+        dropdown.AddOptions(chooseOptions);
+        dropdown.value = resIndex;
+        dropdown.RefreshShownValue();
+
+        /*fullscreen.isOn = Screen.fullScreen;
         vSync.isOn = QualitySettings.vSyncCount != 0;
 
         bool switchLabelToCurrentRes = false;
@@ -40,10 +64,20 @@ public class SettingsScreen : MonoBehaviour {
             currentResIndex = resolutions.Count - 1;
             
             UpdateResolutionLabel();
-        }
-        
+        }*/
+
     }
 
+    public void setFullscreen(bool fullscreen) {
+        Screen.fullScreen = fullscreen;
+    }
+
+    public void setResolution(int resIndex) {
+        Resolution res = resolutionstest[resIndex];
+        Screen.SetResolution(res.width,res.height,Screen.fullScreen);
+    }
+
+    /*
     public void ResolutionSwitchLeft() {
         currentResIndex--;
         if (currentResIndex < 0) {
@@ -70,7 +104,7 @@ public class SettingsScreen : MonoBehaviour {
     public void ApplyGraphicsChanges() {
         QualitySettings.vSyncCount = vSync.isOn ? 1 : 0;
         Screen.SetResolution(resolutions[currentResIndex].horizontal,resolutions[currentResIndex].vertical,fullscreen.isOn); // save 1 line of code by having fullscreen here!
-    }
+    }*/
 }
 // TODO(Elvis): This did not work with an Vector2Int, why? Find out a more appropriate data structure than this class!
 [System.Serializable]
--- the-unravelling/Assets/Scripts/Utility/Singleton Instances/GameData.cs
+++ the-unravelling/Assets/Scripts/Utility/Singleton Instances/GameData.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
-using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
