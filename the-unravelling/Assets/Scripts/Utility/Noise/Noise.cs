2c9b9999fa839a6ffff0307f207f335cf40d6dcb
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-11-06 17:53:35 +0100

    Generate noise into jagged array over multidimensional

--- the-unravelling/Assets/Scripts/Utility/Noise/Noise.cs
+++ the-unravelling/Assets/Scripts/Utility/Noise/Noise.cs
@@ -17,11 +17,11 @@ public static class Noise {
     /// <param name="lacunarity">How much detail each octave adds/removes</param>
     /// <param name="offset">An offset of the noise in (x,y)</param>
     /// <returns>A 2D array of size [mapsize,mapsize] with noise values</returns>
-    public static float[,] generateNoiseMap(Vector2 offset,int mapSize = 256, int seed = 10,
+    public static float[][] generateNoiseMap(Vector2 offset,int mapSize = 256, int seed = 10,
 											float scale = 1,int octaves = 3,float persistance = 0.5f,
 											float lacunarity = 2f, float startFrequency = 1) {
 
-        float[,] noiseMap = new float[mapSize, mapSize];
+        float[][] noiseMap = createJagged2dArray<float>(mapSize,mapSize);
         System.Random pseudo_rng = new System.Random(seed);
     
         Vector2[] octaveOffsets = new Vector2[octaves];
@@ -59,16 +59,30 @@ public static class Noise {
                 } if (noiseHeight < minNoiseHeight) {
                     minNoiseHeight = noiseHeight;
                 }
-                noiseMap[x, y] = noiseHeight;
+                noiseMap[x][y] = noiseHeight;
             }
         }
 
         // Normalizes the noise to be within a 0..1 range
         for (int y = 0; y < mapSize; y++) {
             for (int x = 0; x < mapSize; x++) {
-                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
+                noiseMap[x][y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x][y]);
             }
         }
         return noiseMap;
+    }
+
+	/// <summary>
+    /// Creates a NxM jagged int array 
+    /// </summary>
+    /// <param name="width">The width of the array</param>
+    /// <param name="height">The height of the array</param>
+    /// <returns>The new jagged 2d array</returns>
+    public static T[][] createJagged2dArray<T>(int width, int height) {
+        T[][] array = new T[height][];
+        for (int i = 0; i < array.Length; i++) {
+            array[i] = new T[width];
+        }
+        return array;
     }
 }
