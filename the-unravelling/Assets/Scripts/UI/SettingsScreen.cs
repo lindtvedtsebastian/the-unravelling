1a25492aa00487ef6e924289c0a1ed662296ce8b
Elvis Arifagic <Slewision@gmail.com>
2021-11-06 18:18:14 +0100

    Fullscreen works and resolutions now work on apply button, currently we are not filtering options on resolution but the largest ones are shown first

--- the-unravelling/Assets/Scripts/UI/SettingsScreen.cs
+++ the-unravelling/Assets/Scripts/UI/SettingsScreen.cs
@@ -1,12 +1,10 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using UnityEngine.UI;
 using UnityEngine;
-using Vector2 = UnityEngine.Vector2;
 using TMPro;
-using UnityEngine.InputSystem.OnScreen;
+
 
 public class SettingsScreen : MonoBehaviour {
 
@@ -18,36 +16,15 @@ public class SettingsScreen : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         resolutions = Screen.resolutions; // this varies greatly, on my system it's about 24. This is given to the operating system by the monitor directly
-        List<string> chooseOptions = new List<string>();
+        var chooseOptions = new List<string>();
         var resIndex = 0;
         
-        var highestResolution = resolutions.Last();
-        var test = resolutions.Length-1;
-        var lastres = resolutions[test];
-        Debug.Log(lastres.width + " " + lastres.height + " " + lastres.refreshRate);
+        Array.Reverse(resolutions,0,resolutions.Length);
         
-        
-        var highestRefreshRate = highestResolution.refreshRate;
-
-        // TODO: Make this one work.
-        /*for (var i = 0; i < resolutions.Length; i++) {
-            if (resolutions[i].refreshRate == highestRefreshRate) {
-                var oneResolutionOption = resolutions[i].width + " x " + resolutions[i].height + " " +
-                                          resolutions[i].refreshRate + "hz";
-                chooseOptions.Add(oneResolutionOption);
-
-                if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height) {
-                    resIndex = i;
-                }
-            }
-        }*/
-        
-        // TODO: Why does this work perfectly expect having to many refresh-rates but the one above builds completely wrong
         for (var i = 0; i < resolutions.Length; i++) {
-            var oneResolutionOption = resolutions[i].width + " x " + resolutions[i].height + " " +
-                                      resolutions[i].refreshRate + "hz";
+            var oneResolutionOption = resolutions[i].width + " x " + resolutions[i].height + " " + resolutions[i].refreshRate + "hz";
             chooseOptions.Add(oneResolutionOption);
-
+                
             if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height) { 
                 resIndex = i;
             }
@@ -57,12 +34,11 @@ public class SettingsScreen : MonoBehaviour {
         dropdown.RefreshShownValue();
     }
 
-    public void SetFullscreen(bool fullscreenFlag) {
-        Screen.fullScreen = fullscreenFlag;
-    }
-
-    public void SetResolution(int newResIndex) {
-        var res = resolutions[newResIndex];
-        Screen.SetResolution(res.width,res.height,Screen.fullScreen);
+    public void ApplyGraphicsChanges() {
+        var currentResolutionOnDropdown = dropdown.value;
+        var res = resolutions[currentResolutionOnDropdown];
+        Screen.SetResolution(res.width,res.height,fullscreen.isOn);
+        
+        QualitySettings.vSyncCount = vSync.isOn ? 1 : 0;
     }
 }
