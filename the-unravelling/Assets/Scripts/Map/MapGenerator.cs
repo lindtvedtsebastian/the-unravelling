96a8e6a3f7cb9c2f3997d7af6f20e996bf6ff60b
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-11-12 23:01:31 +0100

    Recreated the old map generation

--- the-unravelling/Assets/Scripts/Map/MapGenerator.cs
+++ the-unravelling/Assets/Scripts/Map/MapGenerator.cs
@@ -2,6 +2,8 @@ using System;
 using UnityEngine;
 
 public static class MapGenerator {
+	private const int MAXVALUE = 100000;
+    private const int MINVALUE = -100000;
 
     public static string currentMapPath;
 
@@ -27,10 +29,10 @@ public static class MapGenerator {
         GameData.Get.world.pathfindingMap = new int[mapSize, mapSize];
         GameData.Get.world.state = new WorldState();
 
-        float[,] heightMap = Noise.generateNoiseMap(mapSize, seed, scale, octaves, persistance, lacunarity,1, offset);
-        float[,] moistureMap = Noise.generateNoiseMap(mapSize, seed + 1, scale, octaves, persistance, lacunarity,1, offset);
-		float[,] resourceClusters = Noise.generateNoiseMap(mapSize, seed + 2, scale, octaves, persistance, lacunarity,1, offset);
-        float[,] resourceDistribution = Noise.generateNoiseMap(mapSize, seed + 3, 20, 1, 1, 1, 10, offset);
+        float[,] heightMap = generateNoiseMap(mapSize, seed, scale, octaves, persistance, lacunarity,1, offset);
+        float[,] moistureMap = generateNoiseMap(mapSize, seed + 1, scale, octaves, persistance, lacunarity,1, offset);
+		float[,] resourceClusters = generateNoiseMap(mapSize, seed + 2, scale, octaves, persistance, lacunarity,1, offset);
+        float[,] resourceDistribution = generateNoiseMap(mapSize, seed + 3, 20, 1, 1, 1, 10, offset);
 
 
         GameData.Get.world.iEntities.Clear();
@@ -78,8 +80,74 @@ public static class MapGenerator {
             }
         }
     }
+    /// <summary>
+    /// Generates a noisemap, that is to be consumed by the tilemap generator 
+    /// </summary> 
+    /// <param name="mapSize">The size of the map as NxN</param>
+    /// <param name="seed">The seed used for generation</param>
+    /// <param name="scale">The noise scale</param>
+    /// <param name="octaves">How many levels of detail</param>
+    /// <param name="persistance">How much each octave contributes to the overall noise</param>
+    /// <param name="lacunarity">How much detail each octave adds/removes</param>
+    /// <param name="offset">An offset of the noise in (x,y)</param>
+    /// <returns>A 2D array of size [mapsize,mapsize] with noise values</returns>
+    public static float[,] generateNoiseMap(int mapSize, int seed, float scale, int octaves,
+                                            float persistance, float lacunarity, float startFrequency, Vector2 offset)
+    {
 
-	public static int determineOreType() {
+        float[,] noiseMap = new float[mapSize, mapSize];
+        System.Random pseudo_rng = new System.Random(seed);
+
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        for (int i = 0; i < octaves; i++)
+        {
+            float offsetX = pseudo_rng.Next(MINVALUE, MAXVALUE) + offset.x;
+            float offsetY = pseudo_rng.Next(MINVALUE, MAXVALUE) + offset.y;
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
+
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
+        float halfSize = mapSize / 2f;
+
+        for (int y = 0; y < mapSize; y++)
+        {
+            for (int x = 0; x < mapSize; x++)
+            {
+
+                float amplitude = 1;
+                float frequency = startFrequency;
+                float noiseHeight = 0;
+
+                for (int i = 0; i < octaves; i++)
+                {
+                    float sampleX = (x - halfSize) / scale * frequency + octaveOffsets[i].x;
+                    float sampleY = (y - halfSize) / scale * frequency + octaveOffsets[i].y;
+
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                    noiseHeight += perlinValue * amplitude;
+
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+                }
+
+                if (noiseHeight > maxNoiseHeight)
+                {
+                    maxNoiseHeight = noiseHeight;
+                }
+                if (noiseHeight < minNoiseHeight)
+                {
+                    minNoiseHeight = noiseHeight;
+                }
+                noiseMap[x, y] = noiseHeight;
+            }
+        }
+
+        return noiseMap;
+    }
+
+    public static int determineOreType() {
         int rand = UnityEngine.Random.Range(0, 10);
 		if (rand < 6)
             return GameIDs.STONE;
@@ -87,38 +155,43 @@ public static class MapGenerator {
             return GameIDs.COPPER_ORE;
 		else
             return GameIDs.IRON_ORE;
-    }
+		}
 
-    /// <summary>
-    /// Determines the maximum noise value in an area around the coordinate
-    /// </summary>
-    /// <param name="x">The base x coordinate</param>
-    /// <param name="y">The base y coordinate</param>
-    /// <param name="radius">The radius around to check</param>
-    /// <param name="noise">The noise array to check in</param>
-    /// <returns>The highest noise value in the area</returns>
-    public static double findMaxAround(int x, int y, int radius, float[,] noise) {
-        double max = 0;
-        for (int iy = y - radius; iy <= y + radius; iy++) {
-            for (int ix = x - radius; ix <= x + radius; ix++) {
-				if (insideMap(iy,ix)) {
-					double value = noise[iy, ix];
-                    max = value > max ? value : max;
+        /// <summary>
+        /// Determines the maximum noise value in an area around the coordinate
+        /// </summary>
+        /// <param name="x">The base x coordinate</param>
+        /// <param name="y">The base y coordinate</param>
+        /// <param name="radius">The radius around to check</param>
+        /// <param name="noise">The noise array to check in</param>
+        /// <returns>The highest noise value in the area</returns>
+        public static double findMaxAround(int x, int y, int radius, float[,] noise)
+        {
+            double max = 0;
+            for (int iy = y - radius; iy <= y + radius; iy++)
+            {
+                for (int ix = x - radius; ix <= x + radius; ix++)
+                {
+                    if (insideMap(iy, ix))
+                    {
+                        double value = noise[iy, ix];
+                        max = value > max ? value : max;
+                    }
                 }
-			}
+            }
+            return max;
         }
-        return max;
-    }
 
-    /// <summary>
-    /// Checks if a coordinate x,y is inside the map
-    /// </summary>
-	/// <param name="x">The x coordinate</param>
-    /// <param name="y">The y coordinate</param>
-    /// <returns>Wheter or not the coord is inside the map</returns>
-    public static bool insideMap(int x, int y) {
-        int size = GameData.Get.world.worldSize;
-        return (x >= 0 && x < size && y >= 0 && y < size);
-    }
+        /// <summary>
+        /// Checks if a coordinate x,y is inside the map
+        /// </summary>
+        /// <param name="x">The x coordinate</param>
+        /// <param name="y">The y coordinate</param>
+        /// <returns>Wheter or not the coord is inside the map</returns>
+        public static bool insideMap(int x, int y)
+        {
+            int size = GameData.Get.world.worldSize;
+            return (x >= 0 && x < size && y >= 0 && y < size);
+        }
 }
 
