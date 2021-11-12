4f31cddc18f5c5bc66ffebf33f443a14c52e9f88
Elvis Arifagic <Slewision@gmail.com>
2021-11-12 11:35:42 +0100

    Deleted main menu prefab which there is no use for currently. Added comments to the audio slider code

--- the-unravelling/Assets/Scripts/Audio/AudioSliderValue.cs
+++ the-unravelling/Assets/Scripts/Audio/AudioSliderValue.cs
@@ -7,49 +7,63 @@ using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class AudioSliderValue : MonoBehaviour {
-    private TMP_Text sliderValue;
-
-    [SerializeField] 
-    string volumeParameter = "MasterVolume";
-    [SerializeField] 
-    AudioMixer mixer;
-    [SerializeField] 
-    Slider slider;
-    [SerializeField]  
-    float sizer;
-    [SerializeField]
-    Toggle masterVolumeToggle;
-
+    public AudioMixer mixer;
+    public Slider slider;
+    public TMP_Text sliderValue;
+    public Toggle muteToggle;
+    
+    public float sizer;
+    public string volumeParameter = "";
+    
     private bool disableToggleEvent;
-
+   
     private void Awake() {
         slider.onValueChanged.AddListener(SliderAudioChanged);
-        masterVolumeToggle.onValueChanged.AddListener(SliderToggleChanged);
+        muteToggle.onValueChanged.AddListener(SliderToggleChanged);
+    }
+        
+    // Start is called before the first frame update
+    void Start() {
+        slider.value = PlayerPrefs.GetFloat(volumeParameter, slider.value);
+    }
+    
+    /// <summary>
+    /// We need to save the volume that the user set
+    /// This way we add our value to the key-value storage of the player preferences.
+    /// </summary>
+    private void OnDisable() {
+        PlayerPrefs.SetFloat(volumeParameter, slider.value);
     }
 
+    /// <summary>
+    /// On enable of this toggle we mute the volume slider and otherwise we go back
+    /// to the audio level that was previous to the mute state.
+    /// </summary>
+    /// <param name="mute">Are we muted or not?</param>
     private void SliderToggleChanged(bool mute) {
         if (disableToggleEvent) 
             return;
         slider.value = mute ? 0.5f : 0.0001f;
     }
     
+    /// <summary>
+    /// Modifies the value of the slider, which is connected to the mixer group
+    /// in turn modifying the volume using the slider.
+    /// </summary>
+    /// <param name="val">Amount to change the volume to</param>
     private void SliderAudioChanged(float val) {
         mixer.SetFloat(volumeParameter, Mathf.Log10(val) * sizer);
         disableToggleEvent = true;
-        masterVolumeToggle.isOn = slider.value > 0.0001f;
+        muteToggle.isOn = slider.value > 0.0001f;
         disableToggleEvent = false;
     }
 
-    private void OnDisable() {
-        PlayerPrefs.SetFloat(volumeParameter, slider.value);
-    }
-    // Start is called before the first frame update
-    void Start() {
-        slider.value = PlayerPrefs.GetFloat(volumeParameter, slider.value);
-        sliderValue = GetComponent<TMP_Text>();
-    }
-
-    /*public void UpdateText(float value) {
+    /// <summary>
+    /// Using this we update the text that is shown as to also give a visual
+    /// indicator to the user of the ui as to what level the audio is at.
+    /// </summary>
+    /// <param name="value">Current value of the slider</param>
+    public void UpdateText(float value) {
         sliderValue.text = Mathf.RoundToInt(value * 100) + "%";
-    }*/
+    }
 }
