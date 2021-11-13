a00499e2ae6b12e0dff86761fe5db7b558ac4c3c
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-11-13 11:12:21 +0100

    Hugely improved way of assigning terrain to a given tile

--- the-unravelling/Assets/Scripts/World/WorldGenerator.cs
+++ the-unravelling/Assets/Scripts/World/WorldGenerator.cs
@@ -5,7 +5,12 @@ using System.Text;
 using UnityEngine;
 
 public class WorldGenerator : MonoBehaviour {
-	public static IWorld generateWorld(int size = 256, int seed = 123) {
+    private const int LOWLAND = 0;
+    private const int HIGHLAND = 10;
+    private const int DRY = 0;
+    private const int MOIST = 5;
+
+    public static IWorld generateWorld(int size = 256, int seed = 123) {
         IWorld world = new IWorld();
         Vector2 offset = new Vector2(0, 0);
         world.size = size;
@@ -13,10 +18,24 @@ public class WorldGenerator : MonoBehaviour {
         float[][] moistureMap = Noise.generateNoiseMap(seed: seed++, offset: offset);
 
 
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
+                world.terrain[y][x] = determineBiome(heightMap[y][x], moistureMap[y][x]);
+            }
+        }
 
-        return world;
+		return world;
     }
 
+
+    private static int determineBiome(float height, float moisture) {
+        int biome = height < 0.4f ? LOWLAND : HIGHLAND;
+        biome += moisture < 0.5f ? DRY : MOIST;
+
+		if (biome < 10) return GameIDs.STONE;
+		else if (biome == 10) return GameIDs.DIRT;
+		else return GameIDs.GRASS;
+    }
 }
 
 [Serializable]
