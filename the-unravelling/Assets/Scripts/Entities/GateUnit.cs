8d652cb51cfd9058c3bacabddc794d3ef3628fea
salvadorbascunan <bascunan.salvador@gmail.com>
2021-12-10 18:24:30 +0100

    changing sprite when gate collider is active. increased box collider radius

--- the-unravelling/Assets/Scripts/Entities/GateUnit.cs
+++ the-unravelling/Assets/Scripts/Entities/GateUnit.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class GateUnit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private SpriteRenderer gateSprite;
+    private BoxCollider2D _collider;
+
+    void Start() {
+        gateSprite = GetComponent<SpriteRenderer>();
+        _collider = GetComponent<BoxCollider2D>();
+
+        _collider.size = new Vector2(5f, 5f);
+    }
+    /// <summary>
+    /// Function to catch the BoxCollider2D trigger enter 
+    /// </summary>
+    /// <param name="col">The collider</param>
+    void OnTriggerEnter2D(Collider2D col) {
+        // Changing the alpha of the sprite when box collider is triggered
+        gateSprite.color = new Color(1f, 1f, 1f, .5f);
+    }
+    /// <summary>
+    /// Function to catch the BoxCollider2D trigger exit
+    /// </summary>
+    /// <param name="col">The collider</param>
+    void OnTriggerExit2D(Collider2D col) {
+        // Changing the alpha back to normal when box collider trigger exit
+        gateSprite.color = new Color(1f, 1f, 1f, 1f);
     }
 }
