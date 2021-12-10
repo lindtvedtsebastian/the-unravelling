777120cbdc82a7b82634248d2586ae2b62747132
salvadorbascunan <bascunan.salvador@gmail.com>
2021-12-10 21:14:08 +0100

    added collider to chest

--- the-unravelling/Assets/Scripts/Inventory/Interactable.cs
+++ the-unravelling/Assets/Scripts/Inventory/Interactable.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interactable : MonoBehaviour
-{
+public class Interactable : MonoBehaviour {
+
+    [SerializeField]
+    private Sprite closedChest;
+
+    [SerializeField]
+    private Sprite openChest;
+
+    private CircleCollider2D _collider;
+    private SpriteRenderer _sprite;
+
+    private float colliderRadius = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _collider = GetComponent<CircleCollider2D>();      
+        _sprite = GetComponent<SpriteRenderer>();  
+
+        _collider.radius = colliderRadius;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
