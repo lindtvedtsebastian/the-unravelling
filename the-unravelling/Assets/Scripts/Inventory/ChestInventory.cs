c743fcf17f34087e5ef7fcc7920f1f3cfd367672
salvadorbascunan <bascunan.salvador@gmail.com>
2021-12-11 16:31:12 +0100

    added chest slot script to differentiate from player item slots in chest inventory

--- the-unravelling/Assets/Scripts/Inventory/ChestInventory.cs
+++ the-unravelling/Assets/Scripts/Inventory/ChestInventory.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChestInventory : MonoBehaviour
-{
+public class ChestInventory : MonoBehaviour {
+    [SerializeField]
+    private GameObject chestInventoryCanvas;
+
     [SerializeField]
     private Inventory _inventory;
 
     public List<Item> chestItems;
     private List<Item> playerItems;
+
+    public Transform _playerPanel;
+    public Transform _chestPanel;
+
+    private ItemSlot[] itemSlots;
+    private CraftingSlot[] craftingSlots;
 
     public ChestInventory() {
         chestItems = new List<Item>();
