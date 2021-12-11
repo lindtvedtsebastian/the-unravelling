fb3adf7c4239a8d6d014f5cffc8c24ae125a21ef
salvadorbascunan <bascunan.salvador@gmail.com>
2021-12-11 14:00:57 +0100

    added chest inventory class

---
+++ the-unravelling/Assets/Scripts/Inventory/ChestInventory.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestInventory : MonoBehaviour
+{
+    [SerializeField]
+    private Inventory playerInventory;
+
+    public List<Item> chestItems;
+    private List<Item> playerItems;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
