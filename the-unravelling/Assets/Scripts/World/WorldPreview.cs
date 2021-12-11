a2fea803970d2e0e20812b8d6049b2f425765f6e
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-12-11 22:15:16 +0100

    New WorldPreview class

---
+++ the-unravelling/Assets/Scripts/World/WorldPreview.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorldPreview : MonoBehaviour {
+    private Texture2D texture;
+    public RawImage mapPreview;
+
+    /// <summary>
+    /// Sets the pixels of a 2D texture to be equivalent to a tilemap
+    /// </summary>
+    public void drawMap(IWorld world) {
+        texture = new Texture2D(world.size, world.size);
+        for (int y = 0; y < world.size; y++) {
+            for (int x = 0; x < world.size; x++) {
+                int tileID = world.terrain[y][x];
+                WorldEntity tile = (WorldEntity) GameData.Get.worldEntities[tileID];
+                texture.SetPixel(x, y,tile.mapColor);
+            }
+        }
+        texture.Apply();
+        mapPreview.texture = texture;
+    }
+}
