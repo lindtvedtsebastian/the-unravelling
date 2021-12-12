fce548de069844a05f40e87e3844ddefee1dfd42
salvadorbascunan <bascunan.salvador@gmail.com>
2021-12-12 22:06:51 +0100

    added hovering over chest entity for testing

--- the-unravelling/Assets/Scripts/Utility/WorldEntityHover.cs
+++ the-unravelling/Assets/Scripts/Utility/WorldEntityHover.cs
@@ -3,34 +3,42 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
-public class WorldEntityHover : EventTrigger {
+public class WorldEntityHover : MonoBehaviour {
     [SerializeField]
-    private Sprite closedChest;
+    private Sprite _normal;
 
     [SerializeField]
-    private Sprite openChest;
+    private Sprite _hovering;
     private SpriteRenderer _sprite;
 
     private EventTrigger _eventTrigger;
 
     void Start() {      
-        _sprite = GetComponent<SpriteRenderer>();  
+        _sprite = GetComponent<SpriteRenderer>(); 
+        _eventTrigger = gameObject.AddComponent<EventTrigger>();
 
+        EventTrigger.Entry enter = new EventTrigger.Entry();
+        enter.eventID = EventTriggerType.PointerEnter;
+        enter.callback.AddListener((data) => {OnPointerEnterDelegate((PointerEventData) data); });
+        _eventTrigger.triggers.Add(enter);
+
+        EventTrigger.Entry exit = new EventTrigger.Entry();
+        exit.eventID = EventTriggerType.PointerExit;
+        exit.callback.AddListener((data) => {OnPointerExitDelegate((PointerEventData) data); });
+        _eventTrigger.triggers.Add(exit);
     }
 
-    public override void OnPointerEnter(PointerEventData data) {
-        
-    }
-
-/*     public void OnHoverEnter() {
+    public void OnPointerEnterDelegate(PointerEventData data) {
+        Debug.Log("Hovering");
         if(_sprite == null) return;
-        _sprite.sprite = openChest;
+
+        _sprite.sprite = _hovering;
     }
 
-    public void OnHoverLeave() {
+    public void OnPointerExitDelegate(PointerEventData data) {
+        Debug.Log("Exit");
         if(_sprite == null) return;
-        _sprite.sprite = closedChest;
-    } */
 
-
+        _sprite.sprite = _normal;
+    }
 }
