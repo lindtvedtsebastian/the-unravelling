d56c4a164760effdb2ebb84eebd93a73c41e82b9
markuszakarias <markus.stromseth@gmail.com>
2021-12-12 12:07:28 +0100

    Merge commit for further development

---
+++ the-unravelling/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Ink.Runtime;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+[RequireComponent(typeof(PlayerInput))]
+public class DialogueManager : MonoBehaviour {
+
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+
+    private Story currentStory;
+
+    [SerializeField] private GameObject[] choices;
+    private TextMeshProUGUI[] choicesText;
+
+    public bool storyIsActive { get; private set; }
+
+    public static DialogueManager instance { get; private set; }
+
+    private void Awake() {
+
+        if (instance != null) {
+            Debug.LogWarning("The DialogueManager is a singleton object. Make sure there are no duplicate instances");
+        }
+
+        instance = this;
+    }
+    
+    private void Start() {
+        storyIsActive = false;
+        dialoguePanel.SetActive(false);
+
+        choicesText = new TextMeshProUGUI[choices.Length];
+
+        var index = 0;
+        foreach (var choice in choices) {
+            choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            index++;
+        }
+    }
+
+    private void Update() {
+        if (!storyIsActive) {
+            return;
+        }
+        if () { // TODO("If player presses the interact button.")
+            ContinueStory();
+        }
+    }
+
+    public void EnterDialogueMode(TextAsset story) {
+        currentStory = new Story(story.text);
+        storyIsActive = true;
+        dialoguePanel.SetActive(true);
+
+        ContinueStory();
+    }
+
+    private void DisplayChoices() {
+        var currentChoices = currentStory.currentChoices;
+
+        if (currentChoices.Count > choices.Length) {
+            Debug.LogError("Unsupported number of choices. Number of choices given: "
+                           + currentChoices.Count);
+        }
+
+        var index = 0;
+        foreach (var choice in currentChoices) {
+            choices[index].gameObject.SetActive(true);
+            choicesText[index].text = choice.text;
+            index++;
+        }
+
+        for (int i = index; i < choices.Length; i++) {
+            choices[i].gameObject.SetActive(false);
+        }
+    }
+
+    private void ContinueStory() {
+        if (currentStory.canContinue) {
+            dialogueText.text = currentStory.Continue();
+            DisplayChoices();
+        }
+        else {
+            StartCoroutine(ExitDialogueMode());
+        }
+    }
+
+    private IEnumerator ExitDialogueMode() {
+        yield return new WaitForSeconds(0.2f);
+        
+        storyIsActive = false;
+        dialoguePanel.SetActive(false);
+        dialogueText.text = "";
+    }
+}
