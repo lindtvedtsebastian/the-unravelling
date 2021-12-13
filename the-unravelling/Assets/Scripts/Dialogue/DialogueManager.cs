45f325268aeaac53b36f6127ea298b2694b7c074
markuszakarias <markus.stromseth@gmail.com>
2021-12-13 18:24:47 +0100

    Added speaker portraits and names with animations

--- the-unravelling/Assets/Scripts/Dialogue/DialogueManager.cs
+++ the-unravelling/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -5,28 +5,39 @@ using Ink.Runtime;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(PlayerInput))]
 public class DialogueManager : MonoBehaviour {
-
+    
+    [Header("Dialogue UI")]
     [SerializeField] private GameObject dialoguePanel;
     [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private TextMeshProUGUI speakerName;
+    [SerializeField] private Animator portraitAnimator;
+    private Animator layoutAnimator;
 
-    private Story currentStory;
-
+    [Header("Choices UI")]
     [SerializeField] private GameObject[] choices;
-    private TextMeshProUGUI[] choicesText;
-
-    [SerializeField] private Button continueButton;
-
-    public bool storyIsActive { get; private set; }
+    private TextMeshProUGUI[] _choicesText;
 
     public static DialogueManager instance { get; private set; }
 
-    private void Awake() {
+    public bool storyIsActive { get; private set; }
+    
+    private Story _currentStory;
 
+    private const string SpeakerTag = "speaker";
+
+    private const string PortraitTag = "portrait";
+
+    private const string LayoutTag = "layout";
+
+    private void Awake() {
+        
+        // Protective check to avoid duplicate instances of a singleton object.
         if (instance != null) {
             Debug.LogWarning("The DialogueManager is a singleton object. Make sure there are no duplicate instances");
         }
@@ -38,11 +49,13 @@ public class DialogueManager : MonoBehaviour {
         storyIsActive = false;
         dialoguePanel.SetActive(false);
 
-        choicesText = new TextMeshProUGUI[choices.Length];
+        layoutAnimator = dialoguePanel.GetComponent<Animator>();
+
+        _choicesText = new TextMeshProUGUI[choices.Length];
 
         var index = 0;
         foreach (var choice in choices) {
-            choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            _choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
             index++;
         }
     }
@@ -57,15 +70,19 @@ public class DialogueManager : MonoBehaviour {
     }
 
     public void EnterDialogueMode(TextAsset story) {
-        currentStory = new Story(story.text);
+        _currentStory = new Story(story.text);
         storyIsActive = true;
         dialoguePanel.SetActive(true);
+
+        speakerName.text = "???";
+        portraitAnimator.Play("default");
+        layoutAnimator.Play("right");
 
         ContinueStory();
     }
 
     private void DisplayChoices() {
-        var currentChoices = currentStory.currentChoices;
+        var currentChoices = _currentStory.currentChoices;
 
         if (currentChoices.Count > choices.Length) {
             Debug.LogError("Unsupported number of choices. Number of choices given: "
@@ -75,7 +92,7 @@ public class DialogueManager : MonoBehaviour {
         var index = 0;
         foreach (var choice in currentChoices) {
             choices[index].gameObject.SetActive(true);
-            choicesText[index].text = choice.text;
+            _choicesText[index].text = choice.text;
             index++;
         }
 
@@ -93,13 +110,44 @@ public class DialogueManager : MonoBehaviour {
     }
 
     public void Choose(int choiceIndex) {
-        currentStory.ChooseChoiceIndex(choiceIndex);
+        _currentStory.ChooseChoiceIndex(choiceIndex);
+    }
+
+    private void HandleTags(List<string> currentTags) {
+        foreach (var tag in currentTags) {
+            string[] splitTag = tag.Split(':');
+
+            if (splitTag.Length != 2) {
+                Debug.LogError("Failed to parse tag: " + tag);
+            }
+
+            string tagKey = splitTag[0].Trim();
+            string tagValue = splitTag[1].Trim();
+
+            switch (tagKey) {
+                case SpeakerTag:
+                    speakerName.text = tagValue;
+                    break;
+                case PortraitTag:
+                    portraitAnimator.Play(tagValue);
+                    break;
+                case LayoutTag:
+                    layoutAnimator.Play(tagValue);
+                    break;
+                default:
+                    Debug.LogWarning("Incorrect handling of tag: " + tag);
+                    break;
+            }
+        }
     }
 
     public void ContinueStory() {
-        if (currentStory.canContinue) {
-            dialogueText.text = currentStory.Continue();
+        if (_currentStory.canContinue) {
+            dialogueText.text = _currentStory.Continue();
+           
             DisplayChoices();
+
+            HandleTags(_currentStory.currentTags);
         }
         else {
             StartCoroutine(ExitDialogueMode());
