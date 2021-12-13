628ece8edc2e631d5abd8af929e06bfbc60ad305
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-12-13 08:49:27 +0100

    Removed unused file

--- the-unravelling/Assets/Scripts/Entities/MachineUnit.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-/// <summary>
-/// Machine unit for `Machine` prefab. See Unity inspector for variables attached to this object.
-/// </summary>
-public class MachineUnit : BaseUnit {
-
-}
