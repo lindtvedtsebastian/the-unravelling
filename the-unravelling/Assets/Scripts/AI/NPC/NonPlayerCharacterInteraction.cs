d8831474a0b1395df4c699654cb245734aa59225
Marius Fredriksen <mkfredri@stud.ntnu.no>
2021-12-15 12:01:09 +0100

    Walking npc have dialogue

---
+++ the-unravelling/Assets/Scripts/AI/NPC/NonPlayerCharacterInteraction.cs
@@ -0,0 +1,17 @@
+﻿
+    
+
+public class NonPlayerCharacterInteraction : State {
+    public override void EnterState(StateManager stateManager) {
+        
+        throw new System.NotImplementedException();
+    }
+
+    public override void DoState() {
+        throw new System.NotImplementedException();
+    }
+
+    public override void LeaveState() {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ the-unravelling/Assets/Scripts/AI/NPC/NonPlayerCharacterWalk.cs
@@ -0,0 +1,105 @@
+﻿
+
+
+
+
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class NonPlayerCharacterWalk : State {
+
+    private Vector3 DirVec;
+    private Transform trans;
+    public float movementSpeed = 2.0f;
+    private Vector2 initPos;
+    
+    
+    private float stuckTime;
+    private float stuckThreshold = 2f;
+    private Vector2 prevPos;
+
+    private float moveTime;
+    private bool isNPCStuck;
+
+    private SpriteRenderer _spriteRenderer;
+    
+    public override void EnterState(StateManager stateManager) {
+        trans = GetComponent<Transform>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        initPos = prevPos = trans.position;
+        DirVec = Vector3.right;
+        MoveNPC();
+    }
+
+    public override void DoState() {
+        stuckTime -= Time.deltaTime;
+ 
+        
+        if (stuckTime <= 0) {
+            
+            if (isStuck()) {
+                trans.position += Vector3.down;
+            }
+            prevPos = gameObject.transform.position;
+            stuckTime = stuckThreshold;
+        }
+        
+        
+        CheckBounds();
+        MoveNPC();
+    }
+
+    public override void LeaveState() {
+        //Nothing to clean up
+        Debug.Log("No cleanup needed");
+    }
+
+
+    private void MoveNPC() {
+        trans.position += DirVec * movementSpeed * Time.deltaTime;
+    }
+
+  
+    /// <summary>
+    /// Creates bounds for the NPC to move within.
+    /// </summary>
+    private void CheckBounds() {
+        if (trans.position.x > initPos.x + 3.0f)
+        {
+            DirVec = Vector3.left;
+            _spriteRenderer.flipX = true;
+        }
+        else if (trans.position.x < initPos.x - 3.0f)
+        {
+            DirVec = Vector3.right;
+            _spriteRenderer.flipX = false;
+        }
+
+    }
+
+    /// <summary>
+    /// Checks if object is stuck 
+    /// </summary>
+    /// <returns>true if stuck</returns>
+    private bool isStuck() {
+        Vector2 currPos = gameObject.transform.position;
+        if (Mathf.Abs(prevPos.x - currPos.x) < .09f)
+            return true;
+        return false;
+    }
+    
+    
+    /// <summary>
+    /// Start interaction state on collision 
+    /// </summary>
+    /// <param name="other">Other gameobject</param>
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Triggered");
+        if (other.gameObject.tag == "Player") {
+            _stateManager.setState(_stateManager.GetComponent<NonPlayerCharacterInteraction>());
+        }
+    }
+    
+    
+}
