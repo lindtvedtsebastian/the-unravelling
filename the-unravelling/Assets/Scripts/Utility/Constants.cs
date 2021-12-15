40a249f183db09b86dea2dfbb60e92e2709acfce
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-12-15 10:52:50 +0100

    Added wood laser for resource gathering

--- the-unravelling/Assets/Scripts/Utility/Constants.cs
+++ the-unravelling/Assets/Scripts/Utility/Constants.cs
@@ -39,6 +39,8 @@ public static class Constants {
 	public const int IRON_COMPONENT = 26;
 	public const int COPPER_COMPONENT = 27;
 
+	public const int WOOD_LASER = 28;
+
 	public static readonly int[] WALLS = {WOOD_WALL, STONE_WALL, IRON_WALL, COPPER_WALL};
 	public static readonly int[] GATES = {WOOD_GATE, STONE_GATE, IRON_GATE, COPPER_GATE};
 
