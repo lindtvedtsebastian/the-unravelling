c3ec2a08b4986f5eda44bc1d27c727fd03a0e112
Marius Fredriksen <mkfredri@stud.ntnu.no>
2021-12-15 18:26:56 +0100

    wordcore integrated with win/loss screen

--- the-unravelling/Assets/Scripts/WorldCore/WinningScreen.cs
+++ the-unravelling/Assets/Scripts/WorldCore/WinningScreen.cs
@@ -10,7 +10,7 @@ public partial class WinningScreen : MonoBehaviour {
     private WorldManager _worldManager;
      
     public void Setup() {
-        _worldManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<WorldManager>();
+        _worldManager = GameObject.FindGameObjectWithTag("WorldManager").GetComponent<WorldManager>();
         gameObject.SetActive(true);
     }
 
