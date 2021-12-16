7557a758b34de7f0527055bfdf3e1b11511c4773
Marius Fredriksen <mkfredri@stud.ntnu.no>
2021-12-16 13:58:36 +0100

    Cleanup and commenting

--- the-unravelling/Assets/Scripts/AI/NPC/NonPlayerCharacterInteraction.cs
+++ the-unravelling/Assets/Scripts/AI/NPC/NonPlayerCharacterInteraction.cs
@@ -3,49 +3,43 @@
 
 using UnityEngine;
 
+/// <summary>
+/// Handles interaction/idle state of the NPC.
+/// </summary>
 public class NonPlayerCharacterInteraction : State {
-   
-    private Vector3 DirVec;
-    private Transform trans;
     
-    private Vector2 initPos;
-    
-
     private DialogueTrigger _dialogueTrigger;
-
     private Animator _animator;
  
-    
+    /// <summary>
+    /// Preparation of state. 
+    /// </summary>
+    /// <param name="stateManager">The statemanager for NPC</param>
     public override void EnterState(StateManager stateManager) {
-        
-        //Get transform
-        trans = GetComponent<Transform>();
-
-        //Set position
-        initPos = trans.position;
-        
-        //Set direction
-        DirVec = Vector3.zero;
-
         //Set animation
         _animator = gameObject.GetComponent<Animator>();
         _animator.Play("Idle");
+        
         //Get dialogue trigger 
         _dialogueTrigger = gameObject.GetComponentInChildren<DialogueTrigger>();
         _stateManager = stateManager;
-        
-        
-        
     }
 
+    /// <summary>
+    /// Doing the state.
+    /// </summary>
     public override void DoState() {
+        //Checking if player is in range og NPC
         if (!_dialogueTrigger.inRange) {
             _stateManager.setState(_stateManager.GetComponent<NonPlayerCharacterWalk>());
         }
-
     }
-
+    
+    /// <summary>
+    /// Cleanup after state.
+    /// </summary>
+    /// <exception cref="NotImplementedException"></exception>
     public override void LeaveState() {
-        throw new System.NotImplementedException();
+        Debug.Log("Nothing to clean up");
     }
 }
